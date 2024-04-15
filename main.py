779926bfa6c95631fbf56fb1685a6bfbb55b9e44
Divy611 <divy.parikh@hotmail.com>
2024-04-15 23:50:44 +0530

    create seperate ml

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 import sqlite3
 import pandas as pd
 import streamlit as st
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-
+from fraud_detection import *
 
 st.set_page_config(page_title='Financial Fraud Detection System', layout='wide',
                    initial_sidebar_state='auto')
@@ -76,125 +71,6 @@ body {
 """
 
 
-def load_data():
-    conn = sqlite3.connect('fraud_data.db')
-    query = "SELECT * FROM transaction_data"
-    df = pd.read_sql(query, conn)
-    conn.close()
-    return df
-
-
-def preprocess_data(df):
-    print("Original DataFrame:")
-    print(df.head())
-    print("Data types before preprocessing:")
-    print(df.dtypes)
-
-    df['transaction_time'] = pd.to_datetime(df['transaction_time'])
-    if 'user_name' in df.columns:
-        df = pd.get_dummies(df, columns=['user_name'], drop_first=True)
-    if 'social_security_number' in df.columns:
-        # Drop social_security_number column
-        df = df.drop(columns=['social_security_number'])
-    numeric_cols = df.select_dtypes(include=['number']).columns
-    imputer = SimpleImputer(strategy='mean')
-    df[numeric_cols] = imputer.fit_transform(df[numeric_cols])
-
-    print("DataFrame after preprocessing:")
-    print(df.head())
-    print("Data types after preprocessing:")
-    print(df.dtypes)
-
-    return df
-
-
-# def preprocess_data(df):
-#     print("Original DataFrame:")
-#     print(df.head())
-#     print("Data types before preprocessing:")
-#     print(df.dtypes)
-#     df['transaction_time'] = pd.to_datetime(df['transaction_time'])
-
-#     if 'user_name' in df.columns:
-#         df = pd.get_dummies(df, columns=['user_name'], drop_first=True)
-#     numeric_cols = df.select_dtypes(include=['number']).columns
-#     imputer = SimpleImputer(strategy='mean')
-#     df[numeric_cols] = imputer.fit_transform(df[numeric_cols])
-#     df['social_security_number'] = df['social_security_number'].astype(
-#         str).fillna('Unknown')
-
-#     encoder = OneHotEncoder(sparse=False)
-#     encoded_ss_numbers = encoder.fit_transform(df[['social_security_number']])
-#     encoded_ss_df = pd.DataFrame(encoded_ss_numbers, columns=[
-#                                  f'social_security_number_{i}' for i in range(encoded_ss_numbers.shape[1])])
-#     df = pd.concat(
-#         [df.drop(columns=['social_security_number']), encoded_ss_df], axis=1)
-
-#     print("DataFrame after preprocessing:")
-#     print(df.head())
-#     print("Data types after preprocessing:")
-#     print(df.dtypes)
-
-#     return df
-
-
-def train_model(df):
-    target_variable = 'transaction_action'
-    if target_variable not in df.columns:
-        raise ValueError(
-            f"Column '{target_variable}' does not exist in DataFrame. Please check your DataFrame columns.")
-    df = preprocess_data(df)
-
-    X = df.drop(columns=[target_variable, 'transaction_time'])
-    y = df[target_variable]
-    X.columns = X.columns.astype(str)
-
-    # Handle missing values
-    imputer = SimpleImputer(strategy='mean')
-    X_imputed = imputer.fit_transform(X)
-    X_train, X_test, y_train, y_test = train_test_split(
-        X_imputed, y, test_size=0.2, random_state=42)
-    clf = RandomForestClassifier(n_estimators=100, random_state=42)
-    clf.fit(X_train, y_train)
-
-    return clf, X_test, y_test
-
-
-def evaluate_model(clf, X_test, y_test):
-    y_pred = clf.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-    precision = precision_score(y_test, y_pred, average='micro')
-    recall = recall_score(y_test, y_pred, average='micro')
-    f1 = f1_score(y_test, y_pred, average='micro')
-
-    return accuracy, precision, recall, f1
-
-
-def load_model():
-    model = RandomForestClassifier()
-    return model
-
-
-def preprocess_input(input_data):
-    df = pd.DataFrame(input_data, index=[0])
-    return df
-
-
-def predict(model, input_data):
-    input_df = preprocess_input(input_data)
-    conn = sqlite3.connect('fraud_data.db')
-    query = "SELECT * FROM transaction_data"
-    df = pd.read_sql(query, conn)
-    conn.close()
-
-    df = preprocess_data(df)
-    X = df.drop(columns=['transaction_action'])
-    y = df['transaction_action']
-    model.fit(X, y)
-    prediction = model.predict(input_df)
-    return prediction
-
-
 def team_page():
     st.title('Our Team')
     st.header("Aditya Pandey (AI-A) (Registration Number: 225890264)")
@@ -219,6 +95,7 @@ def database_page():
 
 
 def main():
+    st.markdown(css, unsafe_allow_html=True)
     if 'app_mode' not in st.session_state:
         st.session_state['app_mode'] = 'main'
     with st.sidebar:
@@ -232,15 +109,12 @@ def main():
         if database_button:
             st.session_state.app_mode = 'database'
             # st.session_state['page'] = 'database'
-    df = load_data()
-    df = preprocess_data(df)
-    st.markdown(css, unsafe_allow_html=True)
     st.title('Financial Fraud Detection System')
     if st.session_state.app_mode == 'team':
         team_page()
     if st.session_state.app_mode == 'database':
         database_page()
-    model = load_model()
+    # model = load_model()
 
     st.write("Enter the transaction details:")
     transaction_time = st.text_input("Time:")
@@ -248,20 +122,24 @@ def main():
     recipient_name = st.text_input("Recipient:")
     transaction_amount = st.number_input("Transaction Amount:")
 
+    # clf, X_test, y_test = train_model(df)
+    if st.button("Predict"):
+        st.write("Evaluating the model...")
+        # accuracy, precision, recall, f1 = evaluate_model(
+        #     clf, X_test, y_test)
+        st.write("Model evaluation complete!")
+        # prediction = predict(model, input_data)
+        # st.write(f"Predicted Transaction Action: {prediction}")
+        st.write("Predicted Transaction Action:")
+
     input_data = {
         'transaction_time': transaction_time,
         'user_name': user_name,
         'recipient_name': recipient_name,
         'transaction_amount': transaction_amount,
     }
-
-    clf, X_test, y_test = train_model(df)
-    if st.button("Predict"):
-        st.write("Evaluating the model...")
-        accuracy, precision, recall, f1 = evaluate_model(clf, X_test, y_test)
-        st.write("Model evaluation complete!")
-        prediction = predict(model, input_data)
-        st.write(f"Predicted Transaction Action: {prediction}")
+    # df = load_data()
+    # df = preprocess_data(df)
 
 
 # Run the app
